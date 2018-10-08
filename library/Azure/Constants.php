0bc34f9b8659111e6b4056c8009bd78da1f463f7
Peter Dreuw <peter.dreuw@credativ.de>
2018-10-08 18:16:15 +0200

    move consts to the appropriate modules and classes

--- library/Azure/Constants.php
+++ library/Azure/Constants.php
@@ -2,9 +2,5 @@
 
 namespace Icinga\Module\Azure;
 
-DEFINE("API_LOGIN",      "https://login.microsoftonline.com");
-DEFINE("API_TOKEN_TYPE", "Bearer");
-DEFINE("API_ENDPT",      "https://management.azure.com/");
-
 
 
